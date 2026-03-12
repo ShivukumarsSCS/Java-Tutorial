7b4df3766b790054a988ccfc5ca4cc9eff77bed1
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-12 18:52:28 +0530

    Create Main1.java

---
+++ Day1/Main1.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String [] args){
+        System.out.println("shivakumar");
+        System.out.println("cse");
+        System.out.println("3rd years");
+    }
+}
