de10b7a838b887c8280e93f29807f50d4ca539e4
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-13 18:40:06 +0530

    Create literals.java

---
+++ Day2/literals.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        short s = 30;
+        float f = 12.1f;
+        double d = 20.22d;
+        char c = 'A';
+        boolean b = true;
+
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
