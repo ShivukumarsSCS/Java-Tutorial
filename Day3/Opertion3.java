fd614491f67a5505498b8d8270bfe3c16e037c5c
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-14 12:34:47 +0530

    Update Opertion3.java

--- Day3/Opertion3.java
+++ Day3/Opertion3.java
@@ -2,7 +2,7 @@ class Airthematic_operation2{
     public static void main(String[] args){
         int a=20;
         int b=40;
-        int sum=a%b;
-        System.out.println(sum);
+        int rem=a%b;
+        System.out.println(rem);
     }
 }
