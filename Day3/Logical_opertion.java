7c38b6ae440f4bbd72ad8fd5630792649b426eac
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-14 12:16:21 +0530

    Create Logical_opertion.java

---
+++ Day3/Logical_opertion.java
@@ -0,0 +1,10 @@
+class Comparission_operation6{
+    public static void main(String[] args){
+        int a=9;
+        int b=5;
+        int c=7;
+        int d=8;
+        System.out.println(a>b&c<d);
+        
+    }
+}
