91be4144871ec4370f924ba5264631278236a35c
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-15 13:56:47 +0530

    Update Test.java

--- Day4/Test.java
+++ Day4/Test.java
@@ -1,6 +1,6 @@
 class Test{
     public static void main(String [] args){
-        String s1="hello";
-        System.out.println(s1);
+        String name="shivu";
+        System.out.println(name);
     }
 }
