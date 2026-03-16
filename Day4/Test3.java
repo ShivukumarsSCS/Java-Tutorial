5d3dd80698d19b490d7cf251e0939970accff5be
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-16 11:10:22 +0530

    Update Test3.java

--- Day4/Test3.java
+++ Day4/Test3.java
@@ -1,7 +1,7 @@
 class Test3{
     public static void main(String [] args){
         String name="kavita";
-        String lstring=name.toUpperCase();
-        System.out.println(lstring);
+        String ustring=name.toUpperCase();
+        System.out.println(ustring);
     }
 }
