e206dfe14c58a43807c855154734d7efa59824d5
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-16 12:34:54 +0530

    Update Char.java

--- Day5/Char.java
+++ Day5/Char.java
@@ -1,7 +1,7 @@
 class Char{
     public static void main(String[] args){
         String name = "kavita";
-        char s = name.charAt(4);
-        System.out.println(s);
+        char c = name.charAt(4);
+        System.out.println(c);
     }
 }
