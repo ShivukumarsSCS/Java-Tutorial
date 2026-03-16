40e2ad99855ca73ce7b8a6a3cbb07cdff284cd26
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-16 12:08:41 +0530

    Create Replace.java

---
+++ Day5/Replace.java
@@ -0,0 +1,7 @@
+class Replace {
+    public static void main(String[] args){
+        String name = "kavita";
+        String rplace = name.replace("kavita", "shivu");
+        System.out.println(rplace);
+    }
+}
