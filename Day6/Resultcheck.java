b8a97a014e027d61065defe2ce5961cc2f5d9d08
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-18 12:14:45 +0530

    Create Resultcheck.java

---
+++ Day6/Resultcheck.java
@@ -0,0 +1,12 @@
+class Resultcheck{
+    public static void main(String [] args){
+        int marks=40;
+        int attendance=80;
+        if(marks>40&&attendance>70){
+            System.out.println("student passed");
+        }else{
+            System.out.println("student failed");
+    }
+        
+    }
+}
