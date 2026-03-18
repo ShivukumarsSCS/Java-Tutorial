e6c243356ba31c66f9b8293d38056299fd3d2b44
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-18 11:49:57 +0530

    Create IF_else.java

---
+++ Day6/IF_else.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String[] args){
+        int age=19;
+        if(age>18){
+            System.out.print("yes you can drive");
+        }else{
+            System.out.println("no you can't drive");
+        }
+    }
+}
