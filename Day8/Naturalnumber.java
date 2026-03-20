89c42b27b20e45987aa6c1a3552264970a9cf27e
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-20 11:12:30 +0530

    Create Naturalnumber.java

---
+++ Day8/Naturalnumber.java
@@ -0,0 +1,10 @@
+class NaturalNumbers {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i <= 1000) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finished printing numbers from 100 to 1000");
+    }
+}
