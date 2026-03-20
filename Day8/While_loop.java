b65d8343b6a8d15e50eb085e27d5bbc7563fdffa
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-20 10:48:41 +0530

    Create While_loop.java

---
+++ Day8/While_loop.java
@@ -0,0 +1,10 @@
+class While_loop{
+    public static void main(String[]args){
+        int i=5;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("finished running of loop");
+    }
+}
