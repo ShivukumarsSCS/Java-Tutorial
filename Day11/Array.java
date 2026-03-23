95265c5937a80d8c95389bcaf65eca6fc9398874
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-23 12:19:32 +0530

    Create Array.java

---
+++ Day11/Array.java
@@ -0,0 +1,17 @@
+class Array{
+    public static void main(String[] args){
+        int [] marks;
+        marks=new int [5];
+        marks[0]=80;
+        marks[1]=70;
+        marks[2]=30;
+        marks[3]=20;
+        marks[4]=50;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+        
+    }
+}
