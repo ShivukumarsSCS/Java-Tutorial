da7ab3f3c8ba923156b09d5b37b432af285a1bb4
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-03-24 12:14:48 +0530

    Create Arrayreverse.java

---
+++ Day12/Arrayreverse.java
@@ -0,0 +1,8 @@
+class Arrayreverse{
+    public static void main(String[]args){
+        int [] marks={87,45,90,20,40};
+        for(int i=marks.length-1;i>=0;i--){
+            System.out.println(marks[i]);
+        }
+    }
+}
