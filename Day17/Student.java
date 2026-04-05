aa60827a146d1769d76436a9168306b5e042f95f
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-04-05 12:44:45 +0530

    Create Student.java

---
+++ Day17/Student.java
@@ -0,0 +1,16 @@
+class Student {
+
+    String name;
+    int marks;
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();   
+
+        s1.name = "shivu";
+        s1.marks = 85;
+
+        System.out.println("Name: " + s1.name);
+        System.out.println("Marks: " + s1.marks);
+    }
+}
