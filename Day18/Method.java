1e1d624ae4fdcd15f910309b886e8364005882fd
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-04-06 17:16:56 +0530

    Create Method.java

---
+++ Day18/Method.java
@@ -0,0 +1,10 @@
+class Demo {
+    void greet() {
+        System.out.println("Hello Shivu!");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();  
+        obj.greet();            
+    }
+}
