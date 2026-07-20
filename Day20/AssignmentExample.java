95722af5d4e264a2b16b3da2d7485b9f17c6ca5f
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-07-20 19:12:56 +0530

    Create AssignmentExample.java

---
+++ Day20/AssignmentExample.java
@@ -0,0 +1,10 @@
+public class AssignmentExample {
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        a += 5;
+
+        System.out.println("Value of a: " + a);
+    }
+}
