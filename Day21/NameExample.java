35fbd63e160f1e74fafbacf74c3f22c4e79c21c5
Shiva Kumar S <shivukumarshivusarjapur@gmail.com>
2026-07-21 22:50:08 +0530

    Create NameExample.java

---
+++ Day21/NameExample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NameExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+
+        String name = sc.next();
+
+        System.out.println("Hello " + name);
+
+        sc.close();
+    }
+}
